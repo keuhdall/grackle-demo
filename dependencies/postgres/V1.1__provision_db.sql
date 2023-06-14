5ed1f6c4785eaa28563ea61070bad1a0cdef1609
keuhdall <keuhdall@gmail.com>
2023-06-14 18:04:19 +0200

    added values for cities table

--- dependencies/postgres/V1.1__provision_db.sql
+++ dependencies/postgres/V1.1__provision_db.sql
@@ -4,3 +4,15 @@ VALUES
 ('England', 'Europe', null, false),
 ('USA', 'North America', 'cheeseburger', false),
 ('Argentina', 'South America', 'empenadas', false);
+
+INSERT INTO cities (name, country_id, is_capital)
+VALUES
+('Paris', 1, true),
+('La Rochelle', 1, false),
+('Bordeaux', 1, false),
+('London', 2, true),
+('Washington', 3, true),
+('New York', 3, false),
+('Seattle', 3, false),
+('San Francisco', 3, false),
+('Buenos Aires', 4, true);
